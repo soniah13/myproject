a9dbbf97aa2e9518a7ab7ba3b1941514000d1bf6
Imorgan Sonia <imorgansonia@gmail.com>
2025-08-21 15:35:02 +0300

    changed on allowed hosts under settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,7 +43,7 @@ cloudinary.config(
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', '').split(',')
 
 
 # Application definition
